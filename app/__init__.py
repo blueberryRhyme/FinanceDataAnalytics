e5159d2569243fc0de44dd99b243d619cd70211a
Jia <23751337@student.uwa.edu.au>
2025-04-12 14:59:00 +0800

    change file structure

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config.from_object('config.Config')
+    
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # Import User model here to avoid circular imports
+    from .models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        #   Flask-Login passes the user ID as a string ?
+        return User.query.get(int(user_id))
+
+
+    # import here to avoid circular import
+    from .routes import main
+    app.register_blueprint(main)
+    
+    # Create database tables if they don't exist
+    with app.app_context():
+        db.create_all()
+    
+    return app
